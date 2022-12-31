9d30744ae7cec2598b710213350428e2463e587c
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2022-12-31 15:17:31 +0200

    Add Init db migration

---
+++ src/ElectricityOffNotifier.Data/ElectricityDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ElectricityOffNotifier.Data;
+
+public sealed class ElectricityDbContextFactory : IDesignTimeDbContextFactory<ElectricityDbContext>
+{
+    public ElectricityDbContext CreateDbContext(string[] args)
+    {
+        var opts = new DbContextOptionsBuilder<ElectricityDbContext>()
+            .UseNpgsql("Host=localhost;")
+            .Options;
+
+        return new ElectricityDbContext(opts);
+    }
+}
