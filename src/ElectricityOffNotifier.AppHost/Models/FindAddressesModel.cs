4c6f915ef95bf51c7324f9a08a3f1093b0580156
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2023-01-09 05:04:17 +0200

    Support for Ukrainian-specific letters

--- src/ElectricityOffNotifier.AppHost/Models/FindAddressesModel.cs
+++ src/ElectricityOffNotifier.AppHost/Models/FindAddressesModel.cs
@@ -7,7 +7,7 @@ public sealed class FindAddressesModel
 {
 	[Range(1, int.MaxValue), FromQuery(Name = "cityId")]
 	public int CityId { get; set; }
-	[Required, RegularExpression("^[а-яА-Яa-zA-Z0-9- .']+$"), FromQuery(Name = "street")]
+	[Required, RegularExpression("^[а-яА-Яa-zA-ZїЇєЄґҐ0-9- .']+$"), FromQuery(Name = "street")]
 	public string Street { get; set; }
 	[Required, RegularExpression("^[0-9a-zA-Zа-яА-Я- /]+$"), FromQuery(Name = "buildingNo")]
 	public string BuildingNo { get; set; }
